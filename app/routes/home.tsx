c39cd6b1c98cbb29fd288a759ae12c723bcdb878
julian57677 <atsejulian@gmail.com>
2025-08-24 21:55:54 +0700

    authentication done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,9 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
-
+import { usePuterStore } from "~/lib/puter";
+import {Link, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,8 +14,16 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+      if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
+
     <section className="main-section">
       <div className="page-heading py-16">
         <h1>Track your resume</h1>
